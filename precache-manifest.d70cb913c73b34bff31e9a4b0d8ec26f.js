7722133bfb457132c763e70e1f6f6677e4cbb657
Ronald Chen <70644931+mraddtab@users.noreply.github.com>
2022-03-10 23:06:08 -0500

    Add files via upload

---
+++ precache-manifest.d70cb913c73b34bff31e9a4b0d8ec26f.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = [
+  {
+    "revision": "dde3c3e677972b44316b",
+    "url": "/static/css/main.ec053e23.chunk.css"
+  },
+  {
+    "revision": "dde3c3e677972b44316b",
+    "url": "/static/js/main.dde3c3e6.chunk.js"
+  },
+  {
+    "revision": "05a8e767b6b683ee5dc9",
+    "url": "/static/js/1.05a8e767.chunk.js"
+  },
+  {
+    "revision": "31c9007a4f719778fbb0",
+    "url": "/static/js/2.31c9007a.chunk.js"
+  },
+  {
+    "revision": "ed64082d111baeb537c6",
+    "url": "/static/js/runtime~main.ed64082d.js"
+  },
+  {
+    "revision": "875334827d633c4f240feb314805702c",
+    "url": "/static/media/LoginButton.87533482.svg"
+  },
+  {
+    "revision": "952f4e60b99e7009bd2469307467055e",
+    "url": "/static/media/Google.952f4e60.svg"
+  },
+  {
+    "revision": "87781a4c6973561f92ccec66907b265e",
+    "url": "/static/media/AppleID.87781a4c.svg"
+  },
+  {
+    "revision": "8cf070f64aabe40f177c2d76a784e816",
+    "url": "/static/media/Facebook.8cf070f6.svg"
+  },
+  {
+    "revision": "fbc1559e123087da42b054070517ba3b",
+    "url": "/static/media/User.fbc1559e.svg"
+  },
+  {
+    "revision": "b1e6480b4c17041e13861c618ccc3487",
+    "url": "/index.html"
+  }
+];
