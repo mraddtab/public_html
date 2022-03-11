c5b34ef5c3f42b123ec496b4fcd3ecba87895659
Ronald Chen <70644931+mraddtab@users.noreply.github.com>
2022-03-10 23:18:25 -0500

    Add files via upload

---
+++ precache-manifest.a2465d62e28527438c056c3246e249d7.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = [
+  {
+    "revision": "dde3c3e677972b44316b",
+    "url": "./static/css/main.ec053e23.chunk.css"
+  },
+  {
+    "revision": "dde3c3e677972b44316b",
+    "url": "./static/js/main.dde3c3e6.chunk.js"
+  },
+  {
+    "revision": "05a8e767b6b683ee5dc9",
+    "url": "./static/js/1.05a8e767.chunk.js"
+  },
+  {
+    "revision": "31c9007a4f719778fbb0",
+    "url": "./static/js/2.31c9007a.chunk.js"
+  },
+  {
+    "revision": "8c6aed3c6cd0dd7deb25",
+    "url": "./static/js/runtime~main.8c6aed3c.js"
+  },
+  {
+    "revision": "875334827d633c4f240feb314805702c",
+    "url": "./static/media/LoginButton.87533482.svg"
+  },
+  {
+    "revision": "952f4e60b99e7009bd2469307467055e",
+    "url": "./static/media/Google.952f4e60.svg"
+  },
+  {
+    "revision": "87781a4c6973561f92ccec66907b265e",
+    "url": "./static/media/AppleID.87781a4c.svg"
+  },
+  {
+    "revision": "8cf070f64aabe40f177c2d76a784e816",
+    "url": "./static/media/Facebook.8cf070f6.svg"
+  },
+  {
+    "revision": "fbc1559e123087da42b054070517ba3b",
+    "url": "./static/media/User.fbc1559e.svg"
+  },
+  {
+    "revision": "16b6ccd83e6fcd6632be65198b1d9b84",
+    "url": "./index.html"
+  }
+];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.d70cb913c73b34bff31e9a4b0d8ec26f.js"
+  "./precache-manifest.a2465d62e28527438c056c3246e249d7.js"
 );
 
 workbox.clientsClaim();
@@ -28,7 +28,7 @@ self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.suppressWarnings();
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute("/index.html", {
+workbox.routing.registerNavigationRoute("./index.html", {
   
   blacklist: [/^\/_/,/\/[^/]+\.[^/]+$/],
 });
